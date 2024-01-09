d46d7a52dedd4e49f6b332f1c5b4df8654b603e8
bottarocarlo <bottarocarloo@gmail.com>
2024-01-09 15:02:16 +0100

    feat: added db and pyenv, endpoint middleware, and first check of teh service name

--- middleware/src/api/endpoints.py
+++ middleware/src/api/endpoints.py
@@ -1,12 +1,19 @@
 # api/endpoints.py
 from flask import request, jsonify
 
-def register_endpoints(app):
+def register_endpoints(app, get_services, save_service, get_service):
     @app.route('/api/v1/get-specs', methods=['GET'])
     def get_specs():
         name_param = request.args.get('name')
 
         print(request.headers)
-        print(f"Parametro GET 'name': {name_param}")
+        print(f"Service name ricevuto da gateway: {name_param}")
 
-        return jsonify({'message': f'Hello {name_param} from the middleware!'})
+        # Verifica se il servizio esiste
+        service = get_service(name_param)
+        if service:
+            # Servizio esiste, esegui la tua logica
+            return service
+        else:
+            # Servizio non esiste
+            return jsonify({'error': 'Servizio non trovato'}), 404
--- middleware/src/main.py
+++ middleware/src/main.py
@@ -4,8 +4,54 @@ from flask_sqlalchemy import SQLAlchemy
 from api.endpoints import register_endpoints
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///services.db'  # Database SQLite
 
-register_endpoints(app)
+db = SQLAlchemy(app)
+
+class Service(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    service_name = db.Column(db.String(255), unique=True, nullable=False)
+    json_data = db.Column(db.JSON, nullable=False)
+
+
+
+# @app.route('/api/v1/get_services', methods=['GET'])
+def get_services():
+    services = Service.query.all()
+    services_list = []
+
+    for service in services:
+        services_list.append({'service_name': service.service_name, 'json_data': service.json_data})
+
+    return jsonify({'services': services_list})
+# @app.route('/api/v1/save_service', methods=['POST'])
+def save_service():
+    data = request.get_json()
+    service_name = data.get('service_name')
+    json_data = data.get('json_data')
+
+    if not service_name or not json_data:
+        return jsonify({'error': 'Nome del servizio o dati mancanti'}), 400
+
+    # Salva nel database
+    new_service = Service(service_name=service_name, json_data=json_data)
+    db.session.add(new_service)
+    db.session.commit()
+
+    return jsonify({'message': 'Servizio salvato con successo'})
+
+# @app.route('/api/v1/get_service/<service_name>', methods=['GET'])
+def get_service(service_name):
+    service = Service.query.filter_by(service_name=service_name).first()
+
+    if service:
+        return jsonify({'service_name': service.service_name, 'json_data': service.json_data})
+    else:
+        return None
+    
+register_endpoints(app ,get_services, save_service, get_service)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
